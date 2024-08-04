de9bdf1c7a8811eeddd5f04c1378dce33b2017f4
ABHI-GUPTA26 <158084578+ABHI-GUPTA26@users.noreply.github.com>
2024-08-04 19:07:09 +0530

    Time: 3 ms (42.96%) | Memory: 7.8 MB (16.02%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
-   int solve(int n){
-    int cnt=0;
-    while(n>0){
-        n=n&(n-1);
-        cnt++;
-    }
-    return cnt;
-
-   }
     int minBitFlips(int start, int goal) {
-        return solve(start^goal);
+        int temp=start^goal;
+        return __builtin_popcount(temp);
     }
 };
