e2cdd4fea871bc529d5a81fcf7cd1258d3d28013
ABHI-GUPTA26 <158084578+ABHI-GUPTA26@users.noreply.github.com>
2024-08-16 11:32:23 +0530

    Time: 128 ms (35.60%) | Memory: 61.8 MB (7.94%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(), position.end());
-        int lo = 1;
-        int hi = (position.back() - position[0]) / (m - 1);
-        int ans = 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (canWePlace(position, mid, m)) {
-                ans = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
+        sort(position.begin(),position.end());
+        int start=1;
+        int end=(position.back()-position[0])/(m-1);
+int ans=1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(canpossible(position,mid,m)){
+                start=mid+1;
+                ans=mid;
+            }else{
+                end=mid-1;
             }
         }
-        return ans;
+        return ans;;
     }
-
-private:
-    bool canWePlace(const vector<int>& arr, int dist, int balls) {
-        int countBalls = 1;
-        int lastPlaced = arr[0];
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr[i] - lastPlaced >= dist) {
-                countBalls++;
-                lastPlaced = arr[i];
+    bool canpossible(vector<int>&position,int dist,int m){
+        int cnt=1;
+        int lastposition=position[0];
+        int n=position.size();
+        for(int i=1;i<n;i++){
+            if(position[i]-lastposition>=dist){
+                cnt++;
+                lastposition=position[i];
             }
-            if (countBalls >= balls) {
-                return true;
-            }
+            if(cnt>=m)return true;
         }
+        
         return false;
     }
 };
