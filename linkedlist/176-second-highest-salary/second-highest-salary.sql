3c1cd3c0cfee51a95114a53b0580aefc5986f73a
ABHI-GUPTA26 <158084578+ABHI-GUPTA26@users.noreply.github.com>
2024-10-13 12:39:43 +0530

    Time: 246 ms (53.06%) | Memory: 0B (100.00%) - LeetSync

--- linkedlist/176-second-highest-salary/second-highest-salary.sql
+++ linkedlist/176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,15 @@
 # Write your MySQL query statement below
 -- select distinct(salary) as SecondHighestSalary from employee as e1 where 1 = (select count(*) from employee as e2 where e1.salary < e2.salary);
 
-select max(salary) as SecondHighestSalary from employee where salary != (
-    select max(salary) from employee
-);
+-- select max(salary) as SecondHighestSalary from employee where salary != (
+--     select max(salary) from employee
+-- );
+
+
+
+SELECT min(salary) as SecondHighestSalary
+FROM (
+    SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS drnk
+    FROM Employee
+) ranked_salaries
+WHERE drnk = 2;
